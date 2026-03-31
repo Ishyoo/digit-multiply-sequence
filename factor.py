e668b961f178152311545ff8047c9872f76890d7
Ishy <ewanevans12@outlook.com>
2026-03-31 03:22:17 +0100

    started

---
+++ factor.py
@@ -0,0 +1,31 @@
+
+def isprime(num):
+    for i in range(2, int((num**0.5))+1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def factors(num):
+    factors = []
+    for i in range(1, int((num**0.5))+1):
+        if num % i == 0:
+            factors.append((i, num//i))
+    return factors
+
+
+def prime_factors(num):
+    solution = []
+    if isprime(num):
+        return [num]
+
+    for factor in factors(num)[-1]:
+        if not isprime(factor):
+            for i in prime_factors(factor):
+                solution.append(i)
+        else:
+            solution.append(factor)
+
+    return solution
+
+print(factors(60))
---
+++ family_trees.py
@@ -0,0 +1,11 @@
+
+import factor as factor
+
+def children(num):
+    factors = [factor.prime_factors(num)]
+    
+            
+    
+    print(factors)
+
+children(75)
